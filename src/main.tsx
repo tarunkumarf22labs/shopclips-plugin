6c4a4bc23ab70ebd60db1e8d640898e950e8e646
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-10-09 19:57:24 +0530

    feat : onload

--- src/main.tsx
+++ src/main.tsx
@@ -10,5 +10,27 @@ define(
 	() => console.log("F22 Plugin cleanup")
 );
 
-let el = document.createElement('f22-plugin')
-document.body.appendChild(el)
+
+function handlevalue() {
+  try {
+    window.onload = async () => {
+      // value.record.forEach((element : any ) => {
+    //   const mainelementsofstories = document.querySelector(`#f22-storiesplugin1`);
+    //   if (mainelementsofstories) {
+        let el = document.createElement("f22-plugin");
+        //@ts-ignore
+        let  result = window.Shopify?.shop?.split(".")[0] || "ekkatha";
+        // let  result = window.Shopify?.shop?.split(".")[0];
+        // console.log(result);
+        // el.setAttribute("dataUrl", `https://s3.f22labs.cloud/shopclips/${result}.json`);
+        el.setAttribute("dataUrl", `https://s3.f22labs.cloud/shopclips/paper-with-video.json`);
+        console.log(el);
+		
+		document.body.appendChild(el)
+      }
+    // };
+  } catch (error) {
+    console.error(error);
+  }
+}
+handlevalue();
