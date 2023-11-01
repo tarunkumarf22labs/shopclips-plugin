0b14d83f41b7471213b6d388c8e2ee491ebfaeb2
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-11-01 14:28:56 +0530

    fix : the url issue

--- src/app.tsx
+++ src/app.tsx
@@ -54,11 +54,11 @@ function App({ dataURL }: { dataURL: string }): JSX.Element {
     try {
       const [storeResponse, clipsResponse] = await Promise.all([
         fetch(
-          `https://shopify-shopclips.uakhui.easypanel.host/api/clips?filters[Store][$contains]=test-for-qa.myshopify.com&populate=deep`,
+          `https://shopify-shopclips.uakhui.easypanel.host/api/clips?filters[Store][$contains]=${window.Shopify.shop}&populate=deep`,
           requestOptions
         ),
         fetch(
-          `https://shopify-shopclips.uakhui.easypanel.host/api/stores?filters[name][$contains]=test-for-qa.myshopify.com`,
+          `https://shopify-shopclips.uakhui.easypanel.host/api/stores?filters[name][$contains]=${window.Shopify.shop}`,
           requestOptions
         )
       ]);
@@ -71,8 +71,7 @@ function App({ dataURL }: { dataURL: string }): JSX.Element {
         url  :  clipsData?.data?.[0].attributes.name ,
         token :  clipsData?.data?.[0].attributes.borderColor
       }  })       
-      const storeValue = storeData.data.find( (data) => data.attributes.clips[0].url === "https://test-for-qa.myshopify.com/"
-      );
+      const storeValue = storeData.data.find( (data) => data.attributes.clips[0].url === window.location.href);
       dataPrecessor(storeValue);
   
       return {
